36a1750f1c994253692c19a1eadfec920dfbc024
Kim Leung <71901468+devworkerkim@users.noreply.github.com>
2021-09-06 21:34:19 -0600

    Minor bug fixes and styling
    docs: update README.md
    fix: win condition for when all squares have been filled
    refactor: substitued null for empty string no that grid size set
    styling: add styling and animation

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ As I was building the objects in the game, I had to think which object a certain
 ### Private Variables
 
 With the way variables are scoped within our objects, we are able to keep our variables private.  When we return our objects, we have the choice to make certain variables accessible to the global environment.  For any game, this prevents users from cheating and manipulating things.  For our Tic-Tac-Toe game, I only allowed access to starting a new game, making moves, and checking if there is a winner.  We can keep our logic private so users couldn't do something like manipulate whose move it is or change what's been played on the board.
+
+### Styling
+
+I thought it would be a cool effect to have the X's and O's look like they were flying into the space.  To achieve this animation effect I had to rearrange some of the HTML elements from their original setup.  Before, the `<div>` elements were the main focus.  When I was applying this effect to the `<div>` elements I found the element spacing to cause some weird lines.  When I scaled out it dragged along the border of the element.  I just wanted the text to have the animation effect.  I went back into the code and added a `<span>` element that contained the text.  From their, I could manipulate the `<span>` without affecting the `<div>`.
+
+I thought it would be easier to work with the animation properties, so to achieve the rotating fly-in I scaled the text to a larger size to make it appear as thought it was traveling away from the screen.  I added a roation animation to give it a cool effect.  With the larger size, however, the text would overflow from its element and cause clipping.  A change in the overflow property of the `<span>` element was needed to make the overflow visible (and without scrollbars).
--- script.js
+++ script.js
@@ -1,35 +1,45 @@
 const game = (() => {
-    let board = [null, null, null, null, null, null, null, null, null];
+    let board = ['','','','','','','','',''];
     let gameComplete = false;
     let player1Turn = true;
     const newBoard = () => {
-        board = new Array(9).fill(null);
+        board = new Array(9).fill('');
         gameComplete = false;
         document.querySelector('.moveMessage').innerText = player1Turn ? "Player X's move" : "Player O's move";
-        document.querySelector('.gameMessage').innerText = 'new game'
+        document.querySelector('.gameMessage').innerText = "New game! Let's play!"
         display();
     }
-    const display = () => {
+    const display = (markedSpace) => {
         while (document.querySelector('.gameBoard').firstChild) {
             document.querySelector('.gameBoard').removeChild(document.querySelector('.gameBoard').firstChild);
         }
         board.forEach((space, ind) => {
             const div = document.createElement('div');
+            div.setAttribute('value', ind);
+            div.setAttribute('tabindex', '0')
             div.onclick = function() {
                 player1Turn ? player1.move(ind) : player2.move(ind);
             }
-            div.innerText = space ? space : '-';
+            div.onkeyup = function(e) {
+                if (e.code === "Space" || e.code === "Enter" || e.code === "NumpadEnter") {
+                    document.querySelector(`div[value="${ind}"]`).click();
+                }
+            }
+            const span = document.createElement('span');
+            if (ind === markedSpace) span.classList.add('spin-in');
+            span.innerText = space ? space : '';
+            div.appendChild(span);
             document.querySelector('.gameBoard').appendChild(div);
         })
     }
 
     const move = (name, type, space) => {
         if (!gameComplete) {
-            if (board[space]) return 'space occupied';
+            if (board[space]) return `Space is occupied. ${player1Turn ? "Player X's" : "Player O's"} move`;
             else {
                 board[space] = type;
                 player1Turn = !player1Turn;
-                display();
+                display(space);
             }
             document.querySelector('.gameMessage').innerText = check();
             return `${name} marked ${type} on space ${space}`;
@@ -44,11 +54,11 @@ const game = (() => {
                 if (board[combo[0]]) gameComplete = true;
             }
         });
-        if (!board.filter((space) => space === null).length) {
+        if (!board.filter((space) => space === '').length && !gameComplete) {
             gameComplete = true;
-            return 'game over (draw)';
+            return 'Game over (draw)';
         }
-        else return gameComplete ? `game over: ${!player1Turn ? player1.getName() : player2.getName()} wins!` : 'continue';
+        else return gameComplete ? `Game over: ${!player1Turn ? player1.getName() : player2.getName()} wins!` : 'Play continues';
     }
 
     return { newBoard, move, check }
@@ -66,9 +76,9 @@ const Player = (name, type) => {
     return { getName, setName, getType, setType, move }
 }
 game.newBoard();
-const player1 = Player('player1', 'X');
+const player1 = Player('Player 1', 'X');
 document.querySelector('#player1Name').value = player1.getName();
-const player2 = Player('player2', 'O');
+const player2 = Player('Player 2', 'O');
 document.querySelector('#player2Name').value = player2.getName();
 
 document.querySelector('#player1Name').addEventListener('input', () => player1.setName(document.querySelector('#player1Name').value));
